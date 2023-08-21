b1ac91403cc53625a6f1723ee84aa4eaf920395b
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-21 20:31:47 +0300

    python-p3-oxford-comma-lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(item):
+    string=''.join(item)
+    if len(item)==2:
+        string=' and '.join(item)
+    elif len(item) > 2:
+        last_item = ', and ' + item[-1]
+        string= ', '.join(item[:-1]) + last_item
+    else:
+        string= ''.join(item)
+    return string
+items=['1','2','3']
+print(oxford_comma(items))
